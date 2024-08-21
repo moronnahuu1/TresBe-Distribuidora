e1f85deb4c6e1f2bc8bde1ce7381cfac8bee3f90
moronnahuu1 <nahuelarielmoron1@gmail.com>
2024-08-21 12:03:23 -0300

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,4 @@ export const environment = {
     production: false,
     endpoint: 'https://tresbe-backend.onrender.com/',
     ///endpoint: 'http://localhost:3000/'
-    admin: process.env["ADMIN"] || 'NOT WORKING'
 };
