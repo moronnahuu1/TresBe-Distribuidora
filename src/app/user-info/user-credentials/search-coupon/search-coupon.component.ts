29de2bb03f1b9d92d83223d4f615d4f8399cb9fa
moronnahuu1 <nahuelarielmoron1@gmail.com>
2024-09-12 12:31:39 -0300

    Cupones en listas

--- src/app/user-info/user-credentials/search-coupon/search-coupon.component.ts
+++ src/app/user-info/user-credentials/search-coupon/search-coupon.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Cupon } from 'src/app/models/Cupon';
 import { CouponService } from 'src/app/services/coupon.service';
 
@@ -7,10 +7,19 @@ import { CouponService } from 'src/app/services/coupon.service';
   templateUrl: './search-coupon.component.html',
   styleUrls: ['./search-coupon.component.css']
 })
-export class SearchCouponComponent {
+export class SearchCouponComponent implements OnInit{
   notFound: boolean = false;
   couponService = inject(CouponService);
   couponSearched: Cupon = new Cupon('','',0,new Date(), false, 0);
+  coupons: Array<Cupon> = [];
+
+  async ngOnInit() {
+      (await this.couponService.readCoupons()).subscribe(result => {
+        this.coupons = result;
+      });
+      this.sortCouponsByExpiration();
+      this.coupons.reverse();
+  }
   async searchCoupon(){
     let inpAux = document.getElementById('couponNameInp') as HTMLInputElement;
     if(inpAux){
@@ -21,6 +30,38 @@ export class SearchCouponComponent {
       }else{
         this.notFound = true;
       }
+    }
+  }
+
+  sortCouponsByExpiration(): void {
+    this.coupons.sort((a, b) => {
+      const dateA = new Date(a.expires);
+      const dateB = new Date(b.expires);
+      return dateA.getTime() - dateB.getTime(); // Ordena en forma ascendente
+    });
+  }
+
+  getDate(isoDate: Date){
+    const date = new Date(isoDate);
+
+// Obtener día, mes y año
+const day = date.getUTCDate().toString().padStart(2, '0'); // Asegura que tenga dos dígitos
+const month = (date.getUTCMonth() + 1).toString().padStart(2, '0'); // Los meses empiezan desde 0
+const year = date.getUTCFullYear().toString();
+
+// Formatear como dd/mm/aaaa
+const formattedDate = `${day}/${month}/${year}`;
+
+return formattedDate;
+  }
+
+  expired(dateAux: Date){
+    let today = new Date();
+    let expireDate = new Date(dateAux);
+    if(today < expireDate){
+      return true;
+    }else{
+      return false;
     }
   }
 }
